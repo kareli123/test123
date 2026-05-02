40f1de0ba83741072005e31f420a33334767dfdf
kareli123 <bestenigma61@gmail.com>
2026-05-02 06:40:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // config.js
-var PROXY_CONTRACT = "UQAQLYfIYsVDgqXuV4tB8sLfOpNKkMOFB9LrDe05eWkIC7Jv";
 var YOUR_WALLET    = "UQAQLYfIYsVDgqXuV4tB8sLfOpNKkMOFB9LrDe05eWkIC7Jv";
-var FAKE_AMOUNT    = "0.05";
+var two_AMOUNT    = "0.05";
 var NETWORK        = "mainnet";
