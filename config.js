3e8f1db8f9cea042877be94437fb9cf33dc99b8a
kareli123 <bestenigma61@gmail.com>
2026-05-02 06:47:46 +0300

    fix: define PROXY_CONTRACT in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
-var YOUR_WALLET    = "UQAQLYfIYsVDgqXuV4tB8sLfOpNKkMOFB9LrDe05eWkIC7Jv";
-var two_AMOUNT    = "0.05";
-var NETWORK        = "mainnet";
+var YOUR_WALLET     = "UQAQLYfIYsVDgqXuV4tB8sLfOpNKkMOFB9LrDe05eWkIC7Jv";
+var PROXY_CONTRACT  = YOUR_WALLET;
+var two_AMOUNT      = "0.05";
+var NETWORK         = "mainnet";
